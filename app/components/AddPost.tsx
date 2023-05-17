ab7b0628350ff047122866eea1e0ff2819714fd2
Nils Matic <matic1909@gmail.com>
2023-05-17 13:11:44 +0200

    fixed exposed env

---
+++ app/components/AddPost.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import axios from "axios";
+
+export default function AddPost() {
+  const [title, setTitle] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  //Create a post
+  const { mutate } = useMutation(
+    async (title) => await axios.post("/api/posts/addPost", { title })
+  );
+
+  const submitPost = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsDisabled(true);
+    mutate(title);
+  };
+
+  return (
+    <form onSubmit={submitPost} className="bg-white my-8 p-8 rounded-md">
+      <div className="flex flex-col my-4">
+        <textarea
+          onChange={(e) => setTitle(e.target.value)}
+          name="title"
+          value={title}
+          placeholder="What's on your mind?"
+          className="p-4 text-lg rounded-md my-2 bg-gray-200"
+        ></textarea>
+      </div>
+      <div className="flex items-center justify-between gap-2`">
+        <p
+          className={`font-bold text-sm ${
+            title.length > 300 ? "text-red-700" : "text-gray-700"
+          }`}
+        >{`${title.length}/300`}</p>
+        <button
+          disabled={isDisabled}
+          className="text-sm bg-teal-600 text-white py-2 px-6 rounded-md disabled:opacity-25"
+          type="submit"
+        >
+          Create a post
+        </button>
+      </div>
+    </form>
+  );
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import Nav from "./auth/Nav";
 import "./globals.css";
 import { Roboto } from "next/font/google";
+import QueryWrapper from "./auth/QueryWrapper";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -17,8 +18,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`mx-4 md:mx-48 lg:mx-96 bg-gray-200 ${roboto.variable}`}>
-        <Nav />
-        {children}
+        <QueryWrapper>
+          <Nav />
+          {children}
+        </QueryWrapper>
       </body>
     </html>
   );
